d9d1d2847d358765ad48534ca3b79a374677f4a5
Noorjahan Abdul <asma@192.168.1.103>
2023-11-27 20:58:32 +0000

    added the Menu

---
+++ script.js
@@ -0,0 +1,89 @@
+const menu_items = [
+    {
+        name: "breakfast One",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 10,
+        food_type: "breakfast"
+    },
+    {
+        name: "breakfast Two",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 10,
+        food_type: "breakfast"
+    },
+    {
+        name: "breakfast Three",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 10,
+        food_type: "breakfast"
+    },
+    {
+        name: "breakfast Four",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 10,
+        food_type: "breakfast"
+    },
+    {
+        name: "lunch One ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "lunch"
+    },
+    {
+        name: "lunch Two ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "lunch"
+    },
+    {
+        name: "lunch Three ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "lunch"
+    },
+    {
+        name: "lunch Four ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "lunch"
+    },
+    {
+        name: "dinner One ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "dinner"
+    },
+    {
+        name: "dinner Two ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "dinner"
+    },
+    {
+        name: "dinner Three ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "dinner"
+    },
+    {
+        name: "dinner Four ",
+        description: "kasdjf asdlfjas dfasdfkjasd;f",
+        image: "",
+        price: 15,
+        food_type: "dinner"
+    }
+];
+
+
+
