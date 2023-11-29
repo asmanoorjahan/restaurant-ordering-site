e5ee0ab584170334e2470a3b6777192fc4333a53
asmanoorjahan <ana.liasma@gmail.com>
2023-11-29 23:06:41 +0000

    functional button completed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const menu_items = [
     {
-        name: "breakfast One",
+        name: "Pancakes",
         description: "kasdjf asdlfjas dfasdfkjasd;f",
-        image: "",
-        price: 10,
+        image: "/images/pancakes.jpeg",
+        price: 15,
         food_type: "breakfast"
     },
     {
@@ -85,5 +85,47 @@ const menu_items = [
     }
 ];
 
+function getItemsHtml(itemslist) {
+    let itemHtml = "";
+    itemslist.forEach(item => {
+        itemHtml = itemHtml +
+            '<article class="menu-item">' +
+            '<img src="' + item.image + '" class="photo" alt="menu item" />' +
+            '<div class="item-info">' +
+            '<header>' +
+            '<h4>' + item.name + '</h4>' +
+            '<h4 class="price">£' + item.price + '</h4>' +
+            '</header>' +
+            '<p class="item-text">' + item.description + '</p>' +
+            '</div>' +
+            '</article>';
+    });
+    return itemHtml;
+}
 
+const mainMenuItemArea = document.getElementById("mainMenuItemArea");
+mainMenuItemArea.innerHTML = getItemsHtml(menu_items);
+
+const allButton = document.getElementById("allButton");
+const breakfastButton = document.getElementById("breakfastButton");
+const lunchButton = document.getElementById("lunchButton");
+const dinnerButton = document.getElementById("dinnerButton");
+
+allButton.addEventListener("click", () => {
+    mainMenuItemArea.innerHTML = getItemsHtml(menu_items);
+});
+
+breakfastButton.addEventListener("click",  () => {
+    breakfastItems = menu_items.filter(item => item.food_type == 'breakfast');
+    mainMenuItemArea.innerHTML = getItemsHtml(breakfastItems);
+});
+
+lunchButton.addEventListener("click",  () => {
+    lunchItems = menu_items.filter(item => item.food_type == 'lunch');
+    mainMenuItemArea.innerHTML = getItemsHtml(lunchItems);
+});
+dinnerButton.addEventListener("click",  () => {
+    dinnerItems = menu_items.filter(item => item.food_type == 'dinner');
+    mainMenuItemArea.innerHTML = getItemsHtml(dinnerItems);
+});
 
